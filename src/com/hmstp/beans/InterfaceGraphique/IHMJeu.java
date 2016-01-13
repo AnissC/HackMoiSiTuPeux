9a8d0c403221df151277c672bcf972b74cd8264f
Dumbeldoge <khaled.bouattour@yahoo.fr>
2016-01-13 18:54:00 +0100

    Interface graphique, panel à reprendre

--- src/com/hmstp/beans/InterfaceGraphique/IHMJeu.java
+++ src/com/hmstp/beans/InterfaceGraphique/IHMJeu.java
@@ -1,7 +1,14 @@
 package com.hmstp.beans.InterfaceGraphique;
 
+
+import com.hmstp.beans.Client.Client;
+import com.sun.deploy.net.proxy.StaticProxyManager;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.StringJoiner;
 
 public class IHMJeu extends JPanel{
     private JFrame frameJeu;//done
@@ -12,6 +19,10 @@ public class IHMJeu extends JPanel{
     private JPanel panelInfo;//done
 
     private JPanel panelBoutons;//done
+
+    private JPanel panelMoi;
+    private JLabel labelMoi;
+    private JButton boutonMoi;
 
     private JPanel panelVictime1;
     private JLabel labelVictime1;
@@ -41,6 +52,8 @@ public class IHMJeu extends JPanel{
     private JLabel labelSeProteger;
     private JButton boutonSeProteger;
 
+    private static final Object[][] rowData = {};
+    private static final Object[] columnNames = {"Joueurs", "Roles","Scores"};
 
     public void start(){
         frameJeu = new JFrame("Hack moi si tu peux");
@@ -71,89 +84,126 @@ public class IHMJeu extends JPanel{
 
 
             //======================Victime 1=============//
+        panelVictime1 = new JPanel();
+        panelVictime1.setOpaque(false);
 
-            panelVictime1 = new JPanel();
-            panelVictime1.setOpaque(false);
-            panelVictime1.setBounds(25,25,100,100);
+        labelVictime1 = new JLabel("Victime 1");
 
-            labelVictime1 = new JLabel("Victime 1");
-            labelVictime1.setForeground(Color.WHITE);
 
-            boutonVictime1 = new JButton("Victime 1");
-            panelVictime1.setLayout(new BorderLayout());
-            panelVictime1.add(labelVictime1, SpringLayout.NORTH);
-            panelVictime1.add(boutonVictime1,SpringLayout.SOUTH);
-            panelBoutons.add(panelVictime1);
+        boutonVictime1 = new JButton("Victime 1");
+        panelVictime1.setLayout(new BorderLayout());
+        panelVictime1.add(labelVictime1, SpringLayout.NORTH);
+        panelVictime1.add(boutonVictime1,SpringLayout.SOUTH);
+        panelBoutons.add(panelVictime1);
 
             //=====================Victime 2==============//
-            panelVictime2 = new JPanel();
-            panelVictime2.setOpaque(false);
+        panelVictime2 = new JPanel();
+        panelVictime2.setOpaque(false);
 
-            labelVictime2 = new JLabel("Victime 2");
-            labelVictime2.setForeground(Color.WHITE);
+        labelVictime2 = new JLabel("Victime 2");
 
-            boutonVictime2 = new JButton("Victme2");
-            panelVictime2.setLayout(new BorderLayout());
-            panelVictime2.add(labelVictime2,BorderLayout.NORTH);
-            panelVictime2.add(boutonVictime2,BorderLayout.SOUTH);
-            panelBoutons.add(panelVictime2);
 
+        boutonVictime2 = new JButton("Victme2");
+        panelVictime2.setLayout(new BorderLayout());
+        panelVictime2.add(labelVictime2,BorderLayout.NORTH);
+        panelVictime2.add(boutonVictime2,BorderLayout.SOUTH);
+        panelBoutons.add(panelVictime2);
 
         //=====================Victime 3==============//
-            panelVictime3 = new JPanel();
-            panelVictime3.setOpaque(false);
+        panelVictime3 = new JPanel();
+        panelVictime3.setOpaque(false);
 
-            labelVictime3 = new JLabel("Victime 3");
-            labelVictime3.setLayout(new BorderLayout());
-            boutonVictime3 = new JButton("Victme3");
+        labelVictime3 = new JLabel("Victime 3");
 
-            panelVictime3.add(labelVictime3,BorderLayout.NORTH);
-            panelVictime3.add(boutonVictime3,BorderLayout.SOUTH);
-            panelVictime3.setBounds(125,25,50,50);
-            //panelBoutons.add(panelVictime3);
+
+        boutonVictime3 = new JButton("Victme3");
+        panelVictime3.setLayout(new BorderLayout());
+        panelVictime3.add(labelVictime3,BorderLayout.NORTH);
+        panelVictime3.add(boutonVictime3,BorderLayout.SOUTH);
+        panelBoutons.add(panelVictime3);
 
         //=====================Victime 4==============//
-            panelVictime4 = new JPanel();
-            panelVictime4.setOpaque(false);
-            labelVictime4 = new JLabel("Victime 4");
-            boutonVictime4 = new JButton("Victme4");
-            panelVictime4.add(labelVictime4,BorderLayout.NORTH);
-            panelVictime4.add(boutonVictime4,BorderLayout.SOUTH);
-            panelVictime4.setBounds(175,25,50,50);
-            //panelBoutons.add(panelVictime4);
+        panelVictime4 = new JPanel();
+        panelVictime4.setOpaque(false);
+
+        labelVictime4 = new JLabel("Victime 4");
+
+
+        boutonVictime4 = new JButton("Victme4");
+        panelVictime4.setLayout(new BorderLayout());
+        panelVictime4.add(labelVictime4,BorderLayout.NORTH);
+        panelVictime4.add(boutonVictime4,BorderLayout.SOUTH);
+        panelBoutons.add(panelVictime4);
 
             //=====================Victime 5==============//
-            panelVictime5 = new JPanel();
-            panelVictime5.setOpaque(false);
-            labelVictime5 = new JLabel("Victime 5");
-            boutonVictime5 = new JButton("Victme5");
+        panelVictime5 = new JPanel();
+        panelVictime5.setOpaque(false);
 
-            panelVictime5.add(labelVictime5,BorderLayout.NORTH);
-            panelVictime5.add(boutonVictime5,BorderLayout.SOUTH);
-            panelVictime5.setBounds(225,25,50,50);
-            //panelBoutons.add(panelVictime5);
+        labelVictime5 = new JLabel("Victime 5");
 
+        boutonVictime5 = new JButton("Victme5");
+        panelVictime5.setLayout(new BorderLayout());
+        panelVictime5.add(labelVictime5,BorderLayout.NORTH);
+        panelVictime5.add(boutonVictime5,BorderLayout.SOUTH);
+        panelBoutons.add(panelVictime5);
 
         //==================Bouton Economiser=============//
-            panelEconomiser = new JPanel();
-            panelEconomiser.setOpaque(false);
-            labelEconomiser = new JLabel("Economiser");
-            boutonEconomiser = new JButton("Economiser");
-            panelEconomiser.add(labelEconomiser,BorderLayout.NORTH);
-            panelEconomiser.add(boutonEconomiser,BorderLayout.SOUTH);
-            panelEconomiser.setBounds(200,100,50,50);
-           // panelBoutons.add(panelEconomiser);
+        panelEconomiser = new JPanel();
+        panelEconomiser.setOpaque(false);
 
+        labelEconomiser = new JLabel("Economiser");
+
+
+        boutonEconomiser = new JButton("Economiser");
+        panelEconomiser.setLayout(new BorderLayout());
+        panelEconomiser.add(labelEconomiser,BorderLayout.NORTH);
+        panelEconomiser.add(boutonEconomiser,BorderLayout.SOUTH);
+        panelBoutons.add(panelEconomiser);
 
         //==============Bouton SeProteger============//
-            panelSeProteger = new JPanel();
-            panelSeProteger.setOpaque(false);
-            labelSeProteger = new JLabel("Se Proteger");
-            boutonSeProteger = new JButton("Se Proteger");
-            panelSeProteger.add(labelSeProteger,BorderLayout.CENTER);
-            panelSeProteger.add(boutonSeProteger);
-            panelSeProteger.setBounds(400,100,50,50);
-            //panelBoutons.add(panelSeProteger);
+        panelSeProteger = new JPanel();
+        panelSeProteger.setOpaque(false);
+
+        labelSeProteger = new JLabel("Se Proteger");
+
+
+        boutonSeProteger = new JButton("Se Proteger");
+        panelSeProteger.setLayout(new BorderLayout());
+        panelSeProteger.add(labelSeProteger,BorderLayout.NORTH);
+        panelSeProteger.add(boutonSeProteger,BorderLayout.SOUTH);
+        panelBoutons.add(panelSeProteger);
+
+        //===================Bouton pour me target=================//
+        panelMoi = new JPanel();
+        panelMoi.setOpaque(false);
+
+        labelMoi = new JLabel("Moi");
+
+        boutonMoi = new JButton("Moi");
+        panelMoi.setLayout(new BorderLayout());
+        panelMoi.add(labelMoi,BorderLayout.NORTH);
+        panelMoi.add(boutonMoi,BorderLayout.SOUTH);
+        panelBoutons.add(panelMoi);
+
+        //=====================Tableau Score =============================//
+        ArrayList<String> joueurs= Client.classement();
+        DefaultTableModel listTableModel;
+        listTableModel = new DefaultTableModel(rowData, columnNames);
+        for (int i = 1; i < joueurs.size(); i++) {
+
+            listTableModel.addRow(new Object[]{joueurs.get(i), Client.valeur(joueurs.get(i)), Client.score(joueurs.get(i))});
+        }
+
+        JTable listTable;
+        listTable = new JTable(listTableModel);
+        listTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        listTable.setCellEditor(null);
+        listTable.setBounds(50,25,200,200);
+
+        JFrame frame = new JFrame();
+        frame.add(listTable);
+        frame.setVisible(true);
+        frame.pack();
 
         //=====================Ajout de tous les panels===================//
         panelJeu.add(panelTableau);
