c1ac65504025749dfa58d8ab40e37a71efe91624
AnissC <aniss.cherkaoui@gmail.com>
2016-01-14 10:20:23 +0100

    Finalisation du projet

--- src/com/hmstp/beans/Serveur/GestionPartie.java
+++ src/com/hmstp/beans/Serveur/GestionPartie.java
@@ -1,15 +1,18 @@
 package com.hmstp.beans.Serveur;
 
+import com.hmstp.beans.Message.*;
+
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class GestionPartie {
-    private ArrayList<Utilisateur> lobby[];
     private static final int JOUEUR_MAX = 6;
     private static final int JOUEUR_MIN = 3;
+    private ArrayList<Utilisateur> lobby[];
     private ArrayList<ArrayList<Utilisateur>> listePartie;
 
     public GestionPartie(){
+        lobby = new ArrayList[JOUEUR_MAX-JOUEUR_MIN + 1];
         for (int i = 0; i<=JOUEUR_MAX-JOUEUR_MIN;i++){
             lobby[i] = new ArrayList<>();
         }
@@ -17,9 +20,14 @@ public class GestionPartie {
     }
 
     public void insererJoueurDansPartie(int nombreJoueur, Socket socket, String nomJoueur){
+        ArrayList<MessageJoueur> listeJoueur = new ArrayList<>();
         int caseLobby = nombreJoueur-JOUEUR_MIN;
         lobby[caseLobby].add(new Utilisateur(socket,nomJoueur));
-        //Prévenir les joueurs de la partie
+        for (int i=0;i<listeJoueur.size();i++){
+            MessageJoueur mj = new MessageJoueur(lobby[caseLobby].get(i).getNom(),lobby[caseLobby].get(i).getSocket().getInetAddress().toString(),Serveur.PARTIE_FINIE);
+            listeJoueur.add(mj);
+        }
+        Serveur.message(new Lettre(new MessagePartie(listeJoueur, Serveur.PARTIE_TROUVE), socket));
         if (lobby[caseLobby].size() >= caseLobby){
             listePartie.add(lobby[caseLobby]);
             //Prévenir que la partie commence
