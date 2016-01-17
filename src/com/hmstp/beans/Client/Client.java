12e8fa808a92ce73a8c4e30f05f4be5b432a121a
AnissC <aniss.cherkaoui@gmail.com>
2016-01-17 10:48:26 +0100

    Finalisation du projet

--- src/com/hmstp/beans/Client/Client.java
+++ src/com/hmstp/beans/Client/Client.java
@@ -27,7 +27,7 @@ public class Client{
     public  boolean premier = false;
 
     //public static String adresseIP = "169.254.129.165";
-    public static String adresseIP = "132.227.125.85";
+    public static String adresseIP = "81.220.196.252";
     public static int port = 1180;
     public  Socket serveur;
 
