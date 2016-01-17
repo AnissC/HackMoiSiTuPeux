5f32b971f81c3932356396f6cecf97af94b2b0dd
Alban5 <alban.blas@free.fr>
2016-01-17 14:45:26 +0100

    Connexion entre joueurs

---
+++ src/com/hmstp/beans/Client/ClientNouveauJoueur.java
@@ -0,0 +1,47 @@
+package com.hmstp.beans.Client;
+
+
+import com.hmstp.beans.Jeu.*;
+import com.hmstp.beans.Message.*;
+
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientNouveauJoueur extends Thread{
+
+    private Partie partie;
+    private ArrayList<Participant> listParticipant;
+    private int joueurEnAttente;
+    private MessageJoueur mej;
+    private int nbjoueur;
+    private Socket socketclient;
+    private Client client;
+
+    public ClientNouveauJoueur(Partie partie, ArrayList<Participant> listParticipant, int joueurEnAttente, MessageJoueur m, int nb, Socket s, Client client){
+        this.partie = partie;
+        this.listParticipant = listParticipant;
+        this.joueurEnAttente = joueurEnAttente;
+        this.mej = m;
+        this.nbjoueur = nb;
+        this.socketclient = s;
+        this.client = client;
+    }
+
+    public void run(){
+        int h = 0;
+        joueurEnAttente++;
+        synchronized (listParticipant) {
+            while ((h < nbjoueur) && (!listParticipant.get(h).isRemplacant() || (listParticipant.get(h).getNom().equals(mej.getNom())))) {
+                h++;
+            }
+            Joueur j = new Joueur(socketclient, mej.getNom());
+            j.setScore(listParticipant.get(h).getScore());
+            j.setRole(listParticipant.get(h).getRole());
+            listParticipant.set(h, j);
+            client.message(new Lettre(new MessageList(partie.perdant, Client.LIST, listParticipant), j.getSock()));
+
+            joueurEnAttente--;
+            listParticipant.notify();
+        }
+    }
+}
